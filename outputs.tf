d515ee250486fb114a32b6f6eff7543dd8ca4853
James McKay <jammycakes@users.noreply.github.com>
2018-02-12 11:54:33 +0000

    Update outputs to make them work with Terraform 0.11

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "cgw_id" {
-  value = "${aws_customer_gateway.default.id}"
+  value = "${element(concat(aws_customer_gateway.default.*.id,list("")),0)}"
 }
 
 output "cgw_ip_address" {
-  value = "${aws_customer_gateway.default.ip_address}"
+  value = "${element(concat(aws_customer_gateway.default.*.ip_address,list("")),0)}"
 }
 
 output "cgw_bgp_asn" {
-  value = "${aws_customer_gateway.default.bgp_asn}"
+  value = "${element(concat(aws_customer_gateway.default.*.bgp_asn,list("")),0)}"
 }
